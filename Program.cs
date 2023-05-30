4a5b305a757144246986ea0c931ffc163d66a435
neonator79 <neonator79@gmail.com>
2023-05-30 23:10:21 +0200

    method Resize
    method Array.Resize

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ejercicio
+{
+    internal class Program
+    {
+        static void Resize (ref int[] myArray, int value)
+        {
+            Array.Resize<int>(ref myArray, value);
+        }
+        static void Main(string[] args)
+        {
+            int [] myArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            Console.Write("Введите размер массива: ");
+            int valueRes = int.Parse(Console.ReadLine());
+                
+            Resize(ref myArr, valueRes);
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                Console.WriteLine(myArr[i]);
+            } 
+            
+
+            
+        }
+    }
+}
